cf450754f7ceabec43fd1f15b2fdfc8e0709952a
sandman45 <msanders45@gmail.com>
2020-10-29 17:55:17 -0600

    omg

--- server/src/main.js
+++ server/src/main.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: `${__dirname}/.env` });
 const { WebpageServer, isWebpageRead } = require('./WebpageServer');
 
 const TCPServer = require('./TCPServer');
-const portNumber = readPortNumberFromArguments();
+// const portNumber = readPortNumberFromArguments();
 
 // const webpageCallbacks = {
 //     onWebpageReady: () => server.send( { type: 'webpage-ready', arg: {} } ),
@@ -18,12 +18,12 @@ const webpageServer = new WebpageServer();
 //     onMessage: command => webpageServer[command.type](command.arg)
 // } );
 
-function readPortNumberFromArguments() {
-    const port = Number(process.argv[2]) || process.env.PORT;
-    if(!port || port < 0 || port >= 65536) {
-        console.error("This script expects a valid port number (>= 0 and < 65536) as argument.");
-        process.exit()
-    }
-
-    return port;
-}
+// function readPortNumberFromArguments() {
+//     const port = Number(process.argv[2]) || process.env.PORT;
+//     if(!port || port < 0 || port >= 65536) {
+//         console.error("This script expects a valid port number (>= 0 and < 65536) as argument.");
+//         process.exit()
+//     }
+//
+//     return port;
+// }
